b0e90433318b5edc4f9c3db8b68e67da8dd1e796
Jianquan Wang <jay.jianquan.wang@gmail.com>
2025-04-02 01:15:05 -0400

    Add products and productCard

--- ecom-frontend/src/App.jsx
+++ ecom-frontend/src/App.jsx
@@ -1,14 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Products from "./components/Products"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className='flex items-center justify-center h-screen bg-gray-800 text-white text-2xl font-bold'>
-      Hello World
+    <div >
+      <Products/>
     </div>
   )
 }
